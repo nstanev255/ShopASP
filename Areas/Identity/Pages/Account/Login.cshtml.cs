dc1dba0d42a000e83ff40972a2ef9eb79ccf0598
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-21 00:19:15 +0300

    Using partial pages, instead of razor components.

---
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopASP.Areas.Identity.Pages.Account;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
