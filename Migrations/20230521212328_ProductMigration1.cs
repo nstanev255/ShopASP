5b1095d689f780558efd26830a9bb887e865d247
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-22 00:23:54 +0300

    Added migrations.

---
+++ Migrations/20230521212328_ProductMigration1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ShopASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductMigration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Developer_DeveloperId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LogoUrl",
+                table: "Platform");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReleaseDate",
+                table: "Products",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlatformId",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeveloperId",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Units",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LogoId",
+                table: "Platform",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Platform_LogoId",
+                table: "Platform",
+                column: "LogoId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platform_Image_LogoId",
+                table: "Platform",
+                column: "LogoId",
+                principalTable: "Image",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Developer_DeveloperId",
+                table: "Products",
+                column: "DeveloperId",
+                principalTable: "Developer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products",
+                column: "PlatformId",
+                principalTable: "Platform",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platform_Image_LogoId",
+                table: "Platform");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Developer_DeveloperId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Platform_LogoId",
+                table: "Platform");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Units",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "LogoId",
+                table: "Platform");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "ReleaseDate",
+                table: "Products",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PlatformId",
+                table: "Products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DeveloperId",
+                table: "Products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LogoUrl",
+                table: "Platform",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Developer_DeveloperId",
+                table: "Products",
+                column: "DeveloperId",
+                principalTable: "Developer",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products",
+                column: "PlatformId",
+                principalTable: "Platform",
+                principalColumn: "Id");
+        }
+    }
+}
