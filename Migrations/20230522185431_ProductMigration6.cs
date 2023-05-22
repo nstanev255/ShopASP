bd3e7444144fd68324982ba2d48f5e72b7a0b298
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-22 21:55:31 +0300

    Applied migrations.

---
+++ Migrations/20230522185431_ProductMigration6.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductMigration6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Genre_Products_ProductId",
+                table: "Genre");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platform_Image_LogoId",
+                table: "Platform");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Platform",
+                table: "Platform");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Platform",
+                newName: "Platforms");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Platform_LogoId",
+                table: "Platforms",
+                newName: "IX_Platforms_LogoId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Genre_ProductId",
+                table: "Genres",
+                newName: "IX_Genres_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Platforms",
+                table: "Platforms",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Genres_Products_ProductId",
+                table: "Genres",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platforms_Image_LogoId",
+                table: "Platforms",
+                column: "LogoId",
+                principalTable: "Image",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Platforms_PlatformId",
+                table: "Products",
+                column: "PlatformId",
+                principalTable: "Platforms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Genres_Products_ProductId",
+                table: "Genres");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Platforms_Image_LogoId",
+                table: "Platforms");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Platforms_PlatformId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Platforms",
+                table: "Platforms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Platforms",
+                newName: "Platform");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Platforms_LogoId",
+                table: "Platform",
+                newName: "IX_Platform_LogoId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Genres_ProductId",
+                table: "Genre",
+                newName: "IX_Genre_ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Platform",
+                table: "Platform",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Genre_Products_ProductId",
+                table: "Genre",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Platform_Image_LogoId",
+                table: "Platform",
+                column: "LogoId",
+                principalTable: "Image",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Platform_PlatformId",
+                table: "Products",
+                column: "PlatformId",
+                principalTable: "Platform",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
