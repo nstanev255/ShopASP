2e40c5ee7f84a4c203473a0146194f16ed575419
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-22 21:48:45 +0300

    Added model for the ProductList view.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ShopASP.Models;
 using ShopASP.Models.Entity;
 using ShopASP.Services;
 
@@ -10,27 +11,35 @@ public class ProductController : Controller
 {
     private ILogger<ProductController> _logger;
     private IProductService _productService;
-    public ProductController(ILogger<ProductController> logger, IProductService productService)
+    private IGenreService _genreService;
+    private IPlatformService _platformService;
+
+    public ProductController(ILogger<ProductController> logger, IProductService productService,
+        IGenreService genreService, IPlatformService platformService)
     {
         _logger = logger;
         _productService = productService;
+        _genreService = genreService;
+        _platformService = platformService;
     }
-    
+
     [AllowAnonymous]
     [HttpGet("{category}")]
     public async Task<IActionResult> ProductList(string category)
     {
         CategoryType parsedCategory;
+
         if (Enum.TryParse(category.ToUpper(), out parsedCategory))
         {
-            _productService.FindAllByCategory(parsedCategory);
-        }
-        else
-        {
-            // Return 404 if we can't parse the enum.
-            return NotFound();
+            var products = _productService.FindAllByCategory(parsedCategory);
+            var allGenres = _genreService.FindAll();
+            var allPlatforms = _platformService.FindAll();
+
+            var model = new ProductListViewModel { Products = products, Genres = allGenres, Platforms = allPlatforms };
+            return View(model: model);
         }
 
-        return View();
+        // Return 404 if we can't parse the enum.
+        return NotFound();
     }
 }
