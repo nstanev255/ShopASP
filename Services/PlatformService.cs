83efb0f392e7d9b30120081e778df87c09aff354
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-22 21:43:12 +0300

    Implemented FindAll method for the platforms.

--- Services/PlatformService.cs
+++ Services/PlatformService.cs
@@ -1,11 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ShopASP.Data;
 using ShopASP.Models.Entity;
 
 namespace ShopASP.Services;
 
 public class PlatformService : IPlatformService
 {
+    private readonly DbSet<Platform> _platformsDao;
+
+    public PlatformService(ApplicationDbContext dbContext)
+    {
+        _platformsDao = dbContext.Platforms;
+    }
     public List<Platform> FindAll()
     {
-        throw new NotImplementedException();
+        return _platformsDao.ToList();
     }
 }
