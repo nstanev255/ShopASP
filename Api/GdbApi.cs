245d331ed5aa7eb58721424ebb204adc80a8a7ef
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-27 20:39:22 +0300

    GDB api code commited.

--- Api/GdbApi.cs
+++ Api/GdbApi.cs
@@ -33,4 +33,20 @@ public class GdbApi
         return JsonConvert.DeserializeObject<List<GenreModel>>(responseString);
     }
 
+    public async Task<List<GameModel>?> Games(long offset)
+    {
+        var content =
+            new StringContent(
+                $"fields *; limit 500; offset {offset}; sort aggregated_rating desc; where aggregated_rating != null;");
+        var fullUrl = $"{_apiUrl}{_gamesUrl}";
+        var response = await _httpClient.PostAsync(fullUrl, content);
+
+        Console.WriteLine("Response Games " + response.StatusCode);
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<List<GameModel>>(responseString);
+
+    }
+
 }
