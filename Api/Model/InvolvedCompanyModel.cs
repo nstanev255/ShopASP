e55bbe15b1c9cde0e7a2b4c938e0c66b87b2fa4b
nstanev255 <nikolay.stanev.255@gmail.com>
2023-05-28 23:47:44 +0300

    Implemented developer seeding.

---
+++ Api/Model/InvolvedCompanyModel.cs
@@ -0,0 +1,8 @@
+namespace ShopASP.Api.Model;
+
+public class InvolvedCompanyModel
+{
+    public int company { get; set; }
+    public bool developer { get; set; }
+    public bool publisher { get; set; }
+}
