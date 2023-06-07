a644863a2de09e28626ff5e137db34b7123d960c
nstanev255 <nikolay.stanev.255@gmail.com>
2023-06-07 20:17:27 +0300

    Added pagination for the products listing page.

--- Models/ProductListViewModel.cs
+++ Models/ProductListViewModel.cs
@@ -5,5 +5,6 @@ namespace ShopASP.Models;
 public class ProductListViewModel
 {
     public List<Product> Products { get; set; }
+    public int AllPages { get; set; }
     public List<Genre> Genres { get; set; }
 }
--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -5,4 +5,5 @@ namespace ShopASP.Services;
 public interface IProductService
 {
     public List<Product> FindAllByCategory(CategoryType categoryType, int page);
+    public Task<int> CountProductsByCategory(CategoryType categoryType);
 }
