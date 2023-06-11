414bdbad39a018bb4c1ea74579806289a8abc960
nstanev255 <nikolay.stanev.255@gmail.com>
2023-06-11 15:08:31 +0300

    Updated the SingleOrder form request.

--- Models/SingleOrderViewModel.cs
+++ Models/SingleOrderViewModel.cs
@@ -5,7 +5,7 @@ namespace ShopASP.Models;
 public class SingleOrderViewModel
 {
     public BasicProduct Product { get; set; }
-    public CategoryType CategoryType { get; set; }
+    public Category Category { get; set; }
     public decimal FinalPrice { get; set; }
 
     public SingleOrderInputModel InputModel { get; set; }
